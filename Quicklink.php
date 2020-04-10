7b0a497ab5c5ea6e08740d42d9c1c7cf6e643900
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-04-11 00:07:26 +0100

    Allow configuration via PHP vars (mainly to allow filtering more pages from being cached, as well as customize the throttle value).

--- Quicklink.php
+++ Quicklink.php
@@ -5,6 +5,14 @@
 class Quicklink {
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+
+		global
+			$quicklinkOptIgnoresRegex,
+			$quicklinkOptThrottle
+		;
+
+		$quicklinkOptThrottle = $quicklinkOptThrottle ?? 3;
+
 		if ( !$skin->getUser()->isLoggedIn() ) {
 			$out->addScript( "<script src=\"https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver\"></script>" );
 
@@ -13,11 +21,17 @@ class Quicklink {
 			$out->addScript(
 "<script>
 if ( navigator.connection && !/\slow-2g|2g|3g/.test( navigator.connection.effectiveType ) ) {
-	window.addEventListener( 'load', () => {
+	window.addEventListener( 'load', function() {
 		quicklink.listen( {
 			el: document.querySelector( '#content' ),
-			ignores: [ ( uri ) => uri.includes( '.php' ) ],
-			throttle: 5,
+			ignores: [ " . ( isset( $quicklinkOptIgnoresRegex )
+				? ( $quicklinkOptIgnoresRegex . ',' ) 
+				: '' ) . "
+				function( uri ) {
+					return uri.includes( '.php' );
+				},
+			],
+			throttle: $quicklinkOptThrottle,
 		} );
 	} );
 }
@@ -27,5 +41,3 @@ if ( navigator.connection && !/\slow-2g|2g|3g/.test( navigator.connection.effect
 	}
 
 }
-
-
